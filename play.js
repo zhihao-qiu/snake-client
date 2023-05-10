3fa1cc6b7fa5347ca7ec809532582c916c010cb8
zhihao-qiu <zhihao.qiu@hotmail.com>
2023-05-10 13:48:31 -0700

    Setup stdin

--- play.js
+++ play.js
@@ -5,3 +5,20 @@ const { connect } = require('./client');
 
 console.log("Connecting ...");
 connect();
+
+const handleUserInput = function(data) {
+  if (data === '\u0003') {
+    process.exit();
+  }
+};
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+
+
+};
