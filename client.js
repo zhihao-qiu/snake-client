0b7a5602aeb08ae818e491ac4282c5818fb081ac
zhihao-qiu <zhihao.qiu@hotmail.com>
2023-05-10 15:09:08 -0700

    Constants Module

--- client.js
+++ client.js
@@ -1,28 +1,26 @@
 const net = require("net");
-const moves = ['up', 'up', 'up', 'up', 'left', 'left', 'left', 'down'];
+const {IP, PORT, INITNAME} = require('./constants')
 
 // establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
-    host: '165.227.47.243', // IP address here,
-    port: '50541'/// PORT number here,
+    host: IP, // IP address here,
+    port: PORT/// PORT number here,
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
   conn.on(('data'), (data) => console.log(data));
-  
-  const movement = function(index) {
-    if (index < moves.length) {
-      conn.write(`Move: ${moves[index]}`);
-      setTimeout(() => { conn.write(`Move: ${moves[index + 1]}`); }, 500);
-    }
-  };
+
+  // const movement = function() {
+    // conn.write(`Move: up`);
+    // setTimeout(movement, 500);
+  // };
   conn.on(('connect'), () => {
     console.log('Successfully connected to game server');
-    conn.write("'Name: ZHQ' to the server, upon connection");
-    movement(0);
+    conn.write(`Name: ${INITNAME}`);
+    // movement();
   });
 
   return conn;
---
+++ constants.js
@@ -0,0 +1,9 @@
+const IP = '165.227.47.243';
+const PORT = '50541';
+const INITNAME = 'ZHQ';
+
+module.exports = {
+  IP,
+  PORT,
+  INITNAME
+};
