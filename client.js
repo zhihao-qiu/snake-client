75372c96869910e26f47021094139d3af867b6f7
zhihao-qiu <zhihao.qiu@hotmail.com>
2023-05-10 12:24:39 -0700

    hard coded movement

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 const net = require("net");
+const moves = ['up', 'up', 'up', 'up', 'left', 'left', 'left', 'down'];
 
 // establishes a connection with the game server
 const connect = function() {
@@ -11,15 +12,23 @@ const connect = function() {
   conn.setEncoding("utf8");
 
   conn.on(('data'), (data) => console.log(data));
-
+  
+  const movement = function(index) {
+    if (index < moves.length) {
+      conn.write(`Move: ${moves[index]}`);
+      setTimeout(() => { conn.write(`Move: ${moves[index + 1]}`); }, 500);
+    }
+  };
   conn.on(('connect'), () => {
     console.log('Successfully connected to game server');
     conn.write("'Name: ZHQ' to the server, upon connection");
+    movement(0);
   });
 
   return conn;
 
 };
+
 
 module.exports = {
   connect
