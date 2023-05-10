fdf6534952e3a421685b2c2b10ca3cb4ae0dc72b
zhihao-qiu <zhihao.qiu@hotmail.com>
2023-05-10 11:50:17 -0700

    move connect function to client.js

--- play.js
+++ play.js
@@ -1,20 +1,7 @@
-const net = require("net");
 
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: '165.227.47.243', // IP address here,
-    port: '50541'/// PORT number here,
-  });
+const { connect } = require('./client');
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  
-  conn.on(('data'), (data) => console.log(data));
-  return conn;
 
-  
-};
 
 console.log("Connecting ...");
 connect();
