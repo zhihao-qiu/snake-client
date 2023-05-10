469b2ae49d19ba0b362e8b9ba9454fd34002766a
zhihao-qiu <zhihao.qiu@hotmail.com>
2023-05-10 14:56:47 -0700

    WASD Movement

--- input.js
+++ input.js
@@ -1,18 +1,43 @@
-const handleUserInput = function(data) {
-  if (data === '\u0003') {
-    process.exit();
-  }
-};
+const wKey = 'Move: up';
+const aKey = 'Move: left';
+const sKey = 'Move: down';
+const dKey = 'Move: right';
 
-const setupInput = function() {
+// Stores the active TCP connection object.
+let connection;
+
+const setupInput = (conn) => {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
+
+  connection = conn;
+
   stdin.on("data", handleUserInput);
   return stdin;
 };
 
+const handleUserInput = function(data) {
+  switch (data) {
+    case '\u0003':
+      process.exit();
+    case 'w':
+      connection.write(`${wKey}`);
+      break;
+    case 'a':
+      connection.write(`${aKey}`);
+      break;
+    case 's':
+      connection.write(`${sKey}`);
+      break;
+    case 'd':
+      connection.write(`${dKey}`);
+      break;
+    default:
+  }
+};
+
 module.exports = {
   setupInput
-}
+};
--- play.js
+++ play.js
@@ -5,6 +5,6 @@ const { setupInput } = require('./input');
 
 
 console.log("Connecting ...");
-connect();
 
-setupInput();
+
+setupInput(connect());
