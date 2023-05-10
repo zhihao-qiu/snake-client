bd2edf938560e6461720adeac11673c1b1fd8897
zhihao-qiu <zhihao.qiu@hotmail.com>
2023-05-10 13:54:40 -0700

    Input Module

--- play.js
+++ play.js
@@ -1,24 +1,10 @@
 
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 
 
 
 console.log("Connecting ...");
 connect();
 
-const handleUserInput = function(data) {
-  if (data === '\u0003') {
-    process.exit();
-  }
-};
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-
-
-};
+setupInput();
